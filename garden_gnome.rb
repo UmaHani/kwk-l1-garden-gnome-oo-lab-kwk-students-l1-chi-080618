cd3ef040c513bc90cb72de1c89ee0c9f9173207c
UmaHani <umahanimohammadi@yahoo.com>
2018-08-14 14:06:58 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,15 @@
 # Code your instances here
+class GardenGnome 
+  attr_accessor :name, :age, :gluten_allergy 
+  def initialize(hat_color = "red")
+    @personality = "evil"
+  end 
+  
+  def gnaw 
+    puts "Gnawing on a tree!!"
+  end 
+  
+  def shout 
+    puts "GNARLY!!"
+  end 
+  
